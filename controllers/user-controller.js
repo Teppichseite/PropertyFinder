7fe3413a7c1c3ad072febbc3659c0f7c400ed548
Teppichseite <teppichop@yahoo.de>
2019-08-21 20:31:31 +0200

    inited user-controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,20 @@
+module.exports = class UserController {
+
+    static findProperties(req, res){
+
+        const longtidude = req.query.long;
+        const latidude = req.query.latidude;
+
+        //call helper
+
+    }
+
+    static execBookingRequest(req, res){
+
+        const propertyObject = req.query.prop;
+
+        //call helper
+
+    }
+
+}

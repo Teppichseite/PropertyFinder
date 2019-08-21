97e425dbf6b7e4b080bca08bf8a1eb52c46ca35a
Teppichseite <teppichop@yahoo.de>
2019-08-21 21:01:08 +0200

    inited api-controller

---
+++ controllers/api-controller.js
@@ -0,0 +1,19 @@
+module.exports = class UserController {
+
+    static findBookingsByPropertyId(req, res){
+
+        const propertyId = req.query.propId;
+
+        //call helper
+
+    }
+
+    static findBookingsByUserId(req, res){
+
+        const propertyId = req.query.propId;
+        
+        //call helper
+
+    }
+
+}

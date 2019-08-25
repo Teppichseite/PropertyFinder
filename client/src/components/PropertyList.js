a9dd3937e73f580181119457547c2aa6132cb5a9
Teppichseite <teppichop@yahoo.de>
2019-08-25 21:52:16 +0200

    added search bar

--- client/src/components/PropertyList.js
+++ client/src/components/PropertyList.js
@@ -2,21 +2,46 @@ import React from 'react'
 import List from '@material-ui/core/List';
 import PropertyItem from './PropertyItem';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import SearchIcon from '@material-ui/icons/Search';
 import '../public/style.css';
 
 export default class PropertyList extends React.Component {
 
     render() {
+        return (
+            <div>
+            <ListItem className="property-item">
+                <ListItemIcon>
+                    <SearchIcon />
+                </ListItemIcon>
+                <input 
+                    className="search-prop-input" 
+                    placeholder="Enter a search term..."
+                    onChange={this.props.onChange}
+                    />
+            </ListItem>
+            {
+                this.displayList()
+            }
+            </div>
+            
+        );
+    }
+
+    displayList(){
         if(this.props.isPending){
-            return (<CircularProgress className="center"/>)
+            return (<CircularProgress className="center"/>);
         }else if(this.props.hasError){
-            return (<h3 className="center">An error occurred :(</h3>)
+            return (<h3 className="center">An error occurred :(</h3>);
         }else{
             return (<List>
             {this.props.properties.map((prop) => (
                 <PropertyItem name={prop} onClick={this.props.onClick} />
             ))}
-            </List>);
+            </List>)
         }
     }
 

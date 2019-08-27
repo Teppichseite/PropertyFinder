0ff33d9782d8b03e85978d8a20df0f641f99527e
Teppichseite <teppichop@yahoo.de>
2019-08-27 05:20:02 +0200

    added date validation

--- server/validators/user-validator.js
+++ server/validators/user-validator.js
@@ -1,23 +1,52 @@
 const { param, body } = require('express-validator');
 
-module.exports = class UserValidator{
+module.exports = class UserValidator {
 
-    static validateFindBookingsByUserId(){
+    static validateFindBookingsByUserId() {
         return [
             param('userId').exists()
         ];
     }
 
-    static validateCreateNewBooking(){
+    static validateCreateNewBooking() {
         return [
             body('property_name').exists().isString(),
             body('longtidude').exists().isFloat(),
             body('latidude').exists().isFloat(),
+            body('fromDate').exists().isString(),
+            body('toDate').exists().isString()
+                .custom(UserValidator.areDatesValid),
             body('city').optional().isString(),
+            body('street').optional().isString(),
             body('url').optional().isString(),
             body('user.name').exists().isString(),
             body('user.email').exists().isEmail()
         ];
+    }
+
+    /**
+     * Checks if the dates are valid dates and if the toDate
+     * is after the fromDate
+     * @param {String} value 
+     * @param {Request} param1.req - Express request object
+     * @returns {boolean}
+     */
+    static areDatesValid(value, { req }) {
+
+        let fromDate = new Date(req.body.fromDate);
+        let toDate = new Date(value);
+
+        //check if dates are valid
+        if (!isNaN(fromDate.getTime()) && !isNaN(toDate.getTime())) {
+
+            //compare dates
+            if (toDate.getTime() > fromDate.getTime()) {
+                return true;
+            }
+
+        }
+
+        return false;
     }
 
 }

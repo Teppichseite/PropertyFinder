626f71c6bc9f520f8f06ee0d446b5bf6328948fa
Teppichseite <teppichop@yahoo.de>
2019-08-28 08:40:15 +0200

    added /build to repo

---
+++ client/build/precache-manifest.840c61dd85a611456876c7650566dcfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a79499e11fa156058cfda874b0d628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e066f4a0eefa83b540d3",
+    "url": "/static/css/main.3fb56537.chunk.css"
+  },
+  {
+    "revision": "9b8a329db4753547df90",
+    "url": "/static/js/2.3ba61f89.chunk.js"
+  },
+  {
+    "revision": "e066f4a0eefa83b540d3",
+    "url": "/static/js/main.1e6942d3.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  }
+]);

e4d1e407736c08bdf3f90f848d564105c01bbba4
Teppichseite <teppichop@yahoo.de>
2019-08-28 08:40:41 +0200

    removed public folder from /server

--- server/app-generator.js
+++ server/app-generator.js
@@ -15,13 +15,13 @@ const path = require('path');
 /**
  * Generates the express app
  */
-module.exports = class AppGenerator{
+module.exports = class AppGenerator {
 
     /**
      * @param {String} mongodbUrl 
      * @param {boolean} logRequests 
      */
-    constructor(mongodbUrl, logRequests){
+    constructor(mongodbUrl, logRequests) {
         this.mongodbUrl = mongodbUrl;
         this.logRequests = logRequests;
         this.app = express();
@@ -32,9 +32,10 @@ module.exports = class AppGenerator{
      * Generates the express app
      * @returns {Promise<Express>}
      */
-    async generate(){
+    async generate() {
         await this.connectToMongoDB();
         this.setupMiddlewares();
+        this.setupCors();
         this.setupRoutes();
         this.setupStaticFileServing();
         return this.app;
@@ -44,31 +45,49 @@ module.exports = class AppGenerator{
      * Start the server
      * call only after generate
      */
-    startServer(){
+    startServer() {
         this.app.listen(8080, '127.0.0.1', () => {
             console.log('HTTP: limehome-project server running on: http://127.0.0.1:8080');
         });
     }
 
-    async connectToMongoDB(){
+    async connectToMongoDB() {
         return mongoose.connect(localConfig.mongodbUrl, {
             useNewUrlParser: true,
             useFindAndModify: false
         });
     }
 
-    setupMiddlewares(){
+    setupMiddlewares() {
         //setup bodyparser
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(bodyParser.json());
 
-        if(this.logRequests){
+        if (this.logRequests) {
             //enable morgan logger
             this.app.use(logger('dev'));
         }
     }
 
-    setupRoutes(){
+    setupCors() {
+        //Enable cors
+        this.app.use(function (req, res, next) {
+            let allowedOrigins = [
+                'http://localhost:3000', 
+                'http://127.0.0.1:8080'];
+            let origin = req.headers.origin;
+            if (allowedOrigins.indexOf(origin) > -1) {
+                res.setHeader('Access-Control-Allow-Origin', origin);
+            }
+
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            res.header('Access-Control-Allow-Credentials', true);
+            return next();
+        });
+    }
+
+    setupRoutes() {
         this.router.use('/users', users);
         this.router.use('/properties', properties);
         this.router.use('/', views);
@@ -76,9 +95,9 @@ module.exports = class AppGenerator{
         this.app.use('/', this.router);
     }
 
-    setupStaticFileServing(){
+    setupStaticFileServing() {
         //static files in ../client/build
-        this.app.use('/', express.static(path.join(__dirname,'../client/build')));
+        this.app.use('/', express.static(path.join(__dirname, '../client/build')));
     }
 
 
